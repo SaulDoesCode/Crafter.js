edbdc46b0875ec00fa26050f718b31b999469571
SaulDoesCode <saulvdw@gmail.com>
2015-11-11 10:29:14 +0200

    wip components

---
+++ src/CraftCookies.js
@@ -0,0 +1,12 @@
+"use strict";
+(document => {
+    var pre = 'craft:' , splitPre = key => key.split(pre)[1] , maxExpire = new Date('Fri, 11 April 9997 23:59:59 UTC') , doc = document;
+
+  /*  Craft.Cookie = {
+      get : key => ,
+      set : (key,val) =>,
+
+    };
+ */
+
+})();
