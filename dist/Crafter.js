87a7250396f1209a5a0c81a9274f9f9f16af704c
SaulDoesCode <saulvdw@gmail.com>
2016-06-23 14:59:20 +0200

    updated builds

--- dist/Crafter.js
+++ dist/Crafter.js
@@ -48,6 +48,14 @@ function _defineProperty(obj, key, value) {
       return arguments.length && toArr(arguments).every(test);
     };
   }
+
+  function has(str, strings, or) {
+    if (isStr(strings)) return strings.split('')[!or ? 'every' : 'some'](str.includes.bind(str));
+  }
+
+  function degloveStr(str) {
+    return isStr(str) && (str.includes('"') || str.includes("'")) ? str.substring(1, str.length - 1) : str;
+  }
   var sI = 'Isync',
     DestructionEvent = new Event('destroy'),
     toArr = Array.from,
@@ -70,7 +78,6 @@ function _defineProperty(obj, key, value) {
       return toString.call(o) === '[object Object]';
     },
     head = doc.head,
-    BracketsRegEx = /(\{\{[\w\.]*\}\})/g,
     RegExps = {
       email: /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i,
       timeString: /^(2[0-3]|[01]?[0-9]):([0-5]?[0-9]):([0-5]?[0-9])$/,
@@ -1823,6 +1830,7 @@ function _defineProperty(obj, key, value) {
     deglove: function(arr) {
       return is.Arraylike(arr) && arr.length == 1 ? arr[0] : arr;
     },
+    degloveStr: degloveStr,
     last: last,
     /**
      * Splits a string at dots "."
@@ -1943,10 +1951,8 @@ function _defineProperty(obj, key, value) {
         for (var i = 0; i < path.length; ++i) {
           path[i] in obj ? obj = obj[path[i]] : obj = undef;
         }
-      } catch (e) {
-        obj = undef;
-      }
-      return obj;
+        return obj;
+      } catch (e) {}
     },
     /**
      * Craft.setDeep  is similar to getDeep it uses a string to reference to a value
@@ -2077,14 +2083,10 @@ function _defineProperty(obj, key, value) {
      * Craft.has checks whether or not strings or arrays contains
      * certain values
      * @param (string|array) str - collection to check
-     * @param (...*) values - values to check for in the collection
+     * @param (string|array) values - values to check for in the collection
+     * @param (=string) or - some or every
      */
-    has: function(str) {
-      if (!str.includes) toArr(str);
-      return Craft.omit(arguments, str).some(function(e) {
-        return str.includes(e);
-      });
-    },
+    has: has,
     /**
      * Omits values from Objects, Strings and Arraylike objects
      * @method omit
@@ -2383,8 +2385,7 @@ function _defineProperty(obj, key, value) {
         else res.text().then(function(code) {
           if (funcexec) {
             try {
-              new Function(code).call(root);
-              pass();
+              pass(new Function(code).call(root));
             } catch (e) {
               fail(e);
             }
@@ -2586,6 +2587,10 @@ function _defineProperty(obj, key, value) {
     }
   }), _defineProperty(_Craft, "getPath", function(path, full) {
     try {
+      if (has(path, "'\"", true)) return degloveStr(path);
+      else if (path == 'true') return true;
+      else if (path == 'false') return false;
+      else if (is.Num(path)) return Number(path);
       var cutbind = cutdot(path),
         prop = last(cutbind),
         first = cutbind[0],
@@ -2678,9 +2683,7 @@ function _defineProperty(obj, key, value) {
           }
         }
       }
-      if (isFunc(config['attr'])) {
-        el.observeAttrs(config['attr']);
-      }
+      if (isFunc(config['attr'])) el.observeAttrs(config['attr']);
       if (isFunc(config['created'])) return config['created'].call(el);
     };
     var _loop = function(_key5) {
@@ -2812,17 +2815,6 @@ function _defineProperty(obj, key, value) {
         element._cah.dw = false;
       }
     }
-  });
-
-  function parseTemplate(html) {
-    return html.replace(BracketsRegEx, function(match, text, offset, string) {
-      var key = match.replace(/{{/, '').replace('}}', ''),
-        val = Craft.getPath(key);
-      return "<o bind=" + key + ">" + (val == undef ? '' : val) + "</o>";
-    });
-  }
-  Craft.customAttr('craft-template', function(element) {
-    element.replace(Craft.dffstr(parseTemplate(element.html())));
   }); // takes in an affected element and scans it for custom attributes
   // then handles the custom attribute if it was registered with Craft.customAttr
   function manageAttr(el, attr) {
