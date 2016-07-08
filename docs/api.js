a25c2458da0b57bf5608137cf42e913ed612327d
SaulDoesCode <saulvdw@gmail.com>
2016-07-08 16:05:45 +0200

    YUI docs is the bomb

---
+++ docs/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Cookies",
+        "Craft",
+        "EventHandler",
+        "Socket",
+        "dom",
+        "is",
+        "router"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
